3898e53bfd01c377cb44f783741323d85ae6f0a6
9beach <9beach@gmail.com>
2021-09-08 19:43:28 +0900

    Update bin/commit.sh

--- bin/commit.sh
+++ bin/commit.sh
@@ -6,7 +6,9 @@ cd "$REPO_PATH"
 
 git commit -am 'Update readme'
 git push
-cp -f README.md ../3e11ceafcf9477b0bf9f6512f8a4b55a/hanspell-rc.md
+
 cd ../3e11ceafcf9477b0bf9f6512f8a4b55a
+git pull
+cp -f "$REPO_PATH/README.md" hanspell-rc.md
 git commit -am 'Update gist'
 git push
